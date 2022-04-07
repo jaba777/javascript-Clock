a5a06809142916cf0637b0b122dd66e36c57a238
jaba777 <tskhovreba777@gmail.com>
2022-04-07 19:28:49 +0100

    build Js

---
+++ app.js
@@ -0,0 +1,35 @@
+let hr = document.querySelector("#hr");
+let mn = document.querySelector("#mn");
+let sc = document.querySelector("#sc");
+
+setInterval(()=>{
+    let day=new Date();
+    let hh=day.getHours() * 30;
+    let mm=day.getMinutes() * 6;
+    let ss = day.getSeconds() * 6;
+
+    hr.style.transform=`rotateZ(${hh + mn / 12}deg)`;
+    mn.style.transform=`rotateZ(${mm}deg)`;
+    sc.style.transform=`rotateZ(${ss}deg)`;
+});
+
+let hours = document.getElementById('hour');
+let minutes = document.getElementById('minutes');
+let second = document.getElementById('seconds');
+
+let h= new Date().getHours();
+let m = new Date().getMinutes();
+let s = new Date().getSeconds();
+
+if(h>12){
+    h=h-12;
+}
+
+h=h<10 ? "0" +h : h;
+m=m<10 ? "0" +m : m;
+s= s<10 ? "0" +s : s;
+
+
+hours.innerHTML=h;
+minutes.innerHTML=m;
+second.innerHTML=s;
